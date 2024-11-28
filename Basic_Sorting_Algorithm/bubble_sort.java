1c98736579915c7e636e61a321791a5c1f962a5b
DevendraMane <devendradineshmane@gmail.com>
2024-11-29 00:24:00 +0530

    bubbel sort complete(I have to write an optimized code for it)

---
+++ Basic_Sorting_Algorithm/bubble_sort.java
@@ -0,0 +1,33 @@
+package Basic_Sorting_Algorithm;
+
+public class bubble_sort {
+    public static void sort(int arr[]) {
+
+        // * loop for counting the turns
+        for (int turn = 0; turn <= arr.length - 2; turn++) {
+            // * loop for getting the final index of the swapping element
+            for (int j = 0; j <= arr.length - 2 - turn; j++) {
+                // ? big bubble should go to the further index
+                if (arr[j] > arr[j + 1]) {
+                    // ? swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i <= arr.length - 1; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 1, 3, 2 };
+        sort(arr); // O(n²)
+        printArr(arr);
+    }
+}
