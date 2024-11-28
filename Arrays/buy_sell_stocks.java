06e99cbafd60a199510865cdf171aadf74e55263
DevendraMane <devendradineshmane@gmail.com>
2024-11-28 12:20:14 +0530

    added commets for stocks question

--- Arrays/buy_sell_stocks.java
+++ Arrays/buy_sell_stocks.java
@@ -2,14 +2,17 @@ package Arrays;
 
 public class buy_sell_stocks {
     public static void makeProfit(int prices[]) {
+        // initialize the required variables
         int buyPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
 
+        // loop for getting the maxProfit
         for (int i = 0; i <= prices.length - 1; i++) {
-            if (buyPrice < prices[i]) {
+            if (buyPrice < prices[i]) { // profit
                 int profit = prices[i] - buyPrice;
                 maxProfit = Math.max(maxProfit, profit);
             } else {
+                // if sellingPrice/currPrice/price[i] < buyPrice
                 buyPrice = prices[i];
             }
         }
