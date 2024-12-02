4804a186df3f7f0fc22197b384f533353fdbf0bf
DevendraMane <devendradineshmane@gmail.com>
2024-12-02 11:06:29 +0530

    counting sort

---
+++ Arrays/rotate_array_counterclockwise.java
@@ -0,0 +1,14 @@
+package Arrays;
+// ? Q.Given an unsorted array arr[]. Rotate the array to the left (counter-clockwise direction) by d steps, where d is a positive integer. Do the mentioned change in the array in place.
+
+public class rotate_array_counterclockwise {
+    public static void rotateArr(int arr[], int dSteps) {
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        int dSteps = 2;
+        rotateArr(arr, dSteps);
+    }
+}
--- Basic_Sorting_Algorithm/counting_sort.java
+++ Basic_Sorting_Algorithm/counting_sort.java
@@ -1,7 +1,37 @@
 package Basic_Sorting_Algorithm;
 
-public class counting_sort {
-    public static void main(String[] args) {
+import java.nio.channels.Pipe.SourceChannel;
 
+import java.util.Arrays;
+
+public class counting_sort {
+    public static void sort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i <= arr.length - 1; i++) {
+            largest = Math.max(largest, arr[i]);
+
+        }
+
+        int count[] = new int[largest + 1];
+        for (int i = 0; i <= arr.length - 1; i++) {
+            count[arr[i]]++;
+        }
+
+        // sorting
+        int j = 0;
+        for (int i = 0; i < count.length - 1; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+
+        System.out.print(Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 4, 1, 3, 2, 4, 3, 7 };
+        sort(arr);
     }
 }
