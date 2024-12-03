15363fb2c294ac30b9c83027c78a205d8421b9e5
DevendraMane <devendradineshmane@gmail.com>
2024-12-03 11:16:49 +0530

    palindrom check

--- Strings/check_palindrom.java
+++ Strings/check_palindrom.java
@@ -1,7 +1,20 @@
 package Strings;
 
 public class check_palindrom {
+
+    public static boolean checkPalindrom(String str) {
+        int n = str.length();
+        for (int i = 0; i < (str.length()) / 2; i++) {
+            if (str.charAt(i) != str.charAt(n - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
+        String str = "racecar";
+        System.out.println(checkPalindrom(str));
 
     }
 }
