c074a0a0927a7ee1c437a20ee51e0ba16767b6e5
DevendraMane <devendradineshmane@gmail.com>
2024-12-06 11:27:53 +0530

    with StringBuider

--- Strings/string_compression.java
+++ Strings/string_compression.java
@@ -2,19 +2,20 @@ package Strings;
 
 public class string_compression {
     public static String compress(String str) {
-        String newStr = new String("");
+        // String newStr = new String("");
+        StringBuilder newStr = new StringBuilder("");
         for (int i = 0; i < str.length(); i++) {
             Integer count = 1;
             while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
                 count++;
                 i++;
             }
-            newStr += str.charAt(i);
+            newStr.append(str.charAt(i));
             if (count > 1) {
-                newStr += count.toString();
+                newStr.append(count.toString());
             }
         }
-        return newStr;
+        return newStr.toString();
     }
 
     public static void main(String[] args) {
