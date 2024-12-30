fb4b3a9417acafa0178046ed736905614b7dc4dd
Devendra Mane <140258830+DevendraMane@users.noreply.github.com>
2024-12-30 10:55:16 +0530

    How to HowToApprochQuestion img

--- rough.java
+++ rough.java
@@ -2,25 +2,28 @@ import java.util.*;
 import java.util.Arrays;
 
 public class rough {
-    public static void hIndex(int[] citations) {
+    public static int hIndex(int[] arr1, int[] arr2, int k) {
         // code here
-        int hIndex = 0;
-        Arrays.sort(citations);
-        for (int i = 0; i < citations.length / 2; i++) {
-            int temp = citations[i];
-            citations[i] = citations[citations.length - 1 - i];
-            citations[citations.length - 1 - i] = temp;
-            if (citations[i] >= i) {
-                hIndex++;
+        int m = arr1.length, n = arr2.length;
+        int[] arr3 = new int[m + n];
+
+        System.arraycopy(arr1, 0, arr3, 0, m);
+        System.arraycopy(arr2, 0, arr3, m, n);
+
+        Arrays.sort(arr3);
+
+        for (int i = 0; i < arr3.length; i++) {
+            if (arr3[i] == k) {
+                return i;
             }
         }
-
-        System.out.println(hIndex);
-
+        return -1;
     }
 
     public static void main(String[] args) {
-        int citations[] = { 5, 1, 2, 4, 1 };
-        hIndex(citations);
+        int arr1[] = { 2, 3, 6, 7, 9 };
+        int arr2[] = { 1, 4, 8, 10 };
+        int k = 5;
+        System.out.println(hIndex(arr1, arr2, k));
     }
 }
